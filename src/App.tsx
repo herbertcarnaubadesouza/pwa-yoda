19dc6c5d9e83b6561ae737eb52fd615f19563957
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2023-07-06 19:25:25 -0300

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,8 @@
 import "firebase/messaging";
-import { getToken } from "firebase/messaging";
-import { useEffect } from "react";
 import "./App.css";
-import { messaging } from "./firebase";
 import logo from "./logo.svg";
 
 function App() {
-  useEffect(() => {
-    const requestPermission = async () => {
-      try {
-        const permission = await Notification.requestPermission();
-        if (permission === "granted") {
-          const currentToken = await getToken(messaging, {
-            vapidKey:
-              "BJ3mvEAQ6SWJ_2XsIITHNQybkbR4VMBf9FL3UXWWplWzkKHLhSvaCrQuKA8uQHn_lbq7OsGCzItPwVVCI-_p_y0",
-          });
-          if (currentToken) {
-            // Envie o token para o seu servidor e atualize a UI, se necessário
-          } else {
-            console.log(
-              "No registration token available. Request permission to generate one."
-            );
-          }
-        }
-      } catch (error) {
-        console.log("An error occurred while retrieving token. ", error);
-      }
-    };
-
-    requestPermission();
-  }, []);
-
   return (
     <div className="App">
       <header className="App-header">
