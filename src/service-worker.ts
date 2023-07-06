b445395c802c533d4cf1d2e34824f87a57a3ac9f
herbertcarnaubadesouza <herbertcarnaubadesouza@gmail.com>
2023-07-06 00:25:04 -0300

    console log

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -78,6 +78,7 @@ registerRoute(
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
 self.addEventListener('message', (event) => {
+  console.log(event.data.text());
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
